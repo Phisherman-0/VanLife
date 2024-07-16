40bcd659e8d80a4d0bc1a3e02d9a3fdbd6634751
Phisherman <117809934+Phisherman-0@users.noreply.github.com>
2024-07-16 12:44:11 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,19 @@ import About from './pages/About'
 import Vans from './pages/Vans'
 import VanDetails from './pages/VansDetails'
 import './App.css'
-import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <Navbar />
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/about' element={<About />}/>
-          <Route path='/vans' element={<Vans />}/>
-          <Route path='/vans/:id' element={<VanDetails/>}/>
+          <Route element={<Layout/>}>
+            <Route path='/' element={<Home />}/>
+            <Route path='/about' element={<About />}/>
+            <Route path='/vans' element={<Vans />}/>
+            <Route path='/vans/:id' element={<VanDetails/>}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
